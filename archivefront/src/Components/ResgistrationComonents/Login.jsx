b5dc28ceb17be4267f0a097c17abda6cbc191ebf
Gehad Omar <gehadomar007@gmail.com>
2024-06-26 22:52:03 +0300

    .

--- archivefront/src/Components/ResgistrationComonents/Login.jsx
+++ archivefront/src/Components/ResgistrationComonents/Login.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch("https://localhost:7103/api/Users/Login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          Email: email,
+          Password: password,
+        }),
+      });
+
+      console.log("Response:", response);
+
+      if (response.ok) {
+        alert(`Login Success: `);
+      } else {
+        const errorText = await response.text();
+        alert(`Login failed: ${response.status} - ${errorText}`);
+      }
+    } catch (error) {
+      console.error("Login error:", error);
+      alert("An error occurred during login.");
+    }
+  };
+
   return (
     <div className="login-page">
-      {" "}
-      {/* Added 'login-page' class */}
       <div className="container">
         <div className="left">
           <div className="header">
@@ -18,16 +48,22 @@ const LoginForm = () => {
               type="email"
               className="form-field animation a3"
               placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
             <input
               type="password"
               className="form-field animation a4"
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
             <p className="animation a5">
               <a href="#">Forgot Password</a>
             </p>
-            <button className="animation a6">LOGIN</button>
+            <button className="animation a6" onClick={handleLogin}>
+              LOGIN
+            </button>
           </div>
         </div>
         <div className="right"></div>
